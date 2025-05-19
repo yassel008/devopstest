14440b3c6e968d9e0fa009e16ff4a3cb03d5c937
Yassel Leonardo Reyes <yassel_08@hotmail.com>
2025-05-19 11:21:18 -0400

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,14 +1,15 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
+
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
     def list(self, request):
-        serializer = self.get_serializer(self.get_queryset(), many = True)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk):
