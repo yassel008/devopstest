9e8fac743e8d7ead5df316b098d5e7b7ace9c3a2
Yassel Leonardo Reyes <yassel_08@hotmail.com>
2025-05-19 11:23:02 -0400

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User  
+        model = User
         fields = '__all__'
