323303e07f74e0da59f4c2cd3a79035893cf546e
Yassel Leonardo Reyes <yassel_08@hotmail.com>
2025-05-20 09:56:36 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "devops-demo-cluster"
+}
