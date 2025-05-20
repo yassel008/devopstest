a67400a4dffd3f0e4b47e24feba96d9209caad71
Yassel Leonardo Reyes <yassel_08@hotmail.com>
2025-05-20 13:10:06 -0400

    Update urls.py

--- demo/urls.py
+++ demo/urls.py
@@ -1,11 +1,10 @@
-# demo/urls.py
 from django.urls import path, include
 from rest_framework import routers
-from api.views import UserViewSet  # importa desde 'api', no desde 'demo'
+from api.views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),  # Prefijo /api/
 ]
