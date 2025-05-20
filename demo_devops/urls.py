2f322c8940ce5e45bf6e6709793d7f85c09dfb95
Yassel Leonardo Reyes <yassel_08@hotmail.com>
2025-05-20 13:18:52 -0400

    Update urls.py

--- demo_devops/urls.py
+++ demo_devops/urls.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-from django.urls import path
-urlpatterns = [path('admin/', admin.site.urls)]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
